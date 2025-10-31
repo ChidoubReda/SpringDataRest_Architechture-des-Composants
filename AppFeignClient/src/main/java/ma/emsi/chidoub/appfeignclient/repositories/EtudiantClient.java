7081d6697de4ade4142f0cf690726490a56f4db9
ChidoubReda <Reda.Chidoub@emsi-edu.ma>
2025-10-31 12:35:39 +0100

    Adding AppFeignClient & AppRestTemplate & AppWebClient

---
+++ AppFeignClient/src/main/java/ma/emsi/chidoub/appfeignclient/repositories/EtudiantClient.java
@@ -0,0 +1,13 @@
+package ma.emsi.chidoub.appfeignclient.repositories;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@FeignClient(name = "etudiant-service", url = "http://localhost:8080")
+public interface EtudiantClient {
+    @GetMapping(value="/etudiants", produces = "application/json")
+    Map<String, Object> getAllEtudiants();
+
+}
